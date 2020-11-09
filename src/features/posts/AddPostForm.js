7676e3467518f7167689262a35a0c8ebf3f53991
gong hu <nzchanggong@hotmail.com>
2020-11-10 08:29:43 +1300

    BUTTON工作

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,11 +1,32 @@
+
 import React, { useState } from 'react'
+import { postAdded } from './postsSlice'
+import {nanoid} from '@reduxjs/toolkit'
+import {useDispatch} from 'react-redux'
+
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
+  const dispatch = useDispatch()
+
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),         //nanoid() function generated a random unique ID
+          title,
+          content
+        })
+      )
+      setTitle('')
+      setContent('')
+    }
+  }
 
   return (
     <section>
@@ -26,7 +47,7 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button">Save Post</button>
+        <button type="button" onClick={onSavePostClicked}>Save Post</button>
       </form>
     </section>
   )
