f4d8477bf4d68e989d7106acadaf301a55bd25a8
gong hu <nzchanggong@hotmail.com>
2020-11-09 18:57:51 +1300

    11

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import postsReducer from '../features/posts/postsSlice'
+
+export default configureStore({
+  reducer: {
+    posts: postsReducer
+  }
+})
+
+
+
+
+/*
 export default configureStore({
   reducer: () => ({}),
 })
+*/
