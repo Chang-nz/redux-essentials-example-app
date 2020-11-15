792ca4215e9f9339adc96d7d02f76e71d2a00e52
gong hu <nzchanggong@hotmail.com>
2020-11-15 15:45:48 +1300

    空数据

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,28 +1,11 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
-import { sub } from 'date-fns'
 
-const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!',user: '0', 
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      hooray: 0,
-      heart: 0,
-      rocket: 0,
-      eyes: 0,
-    },
-  },
-  { id: '2', title: 'Second Post', content: 'More text',user: '1', 
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      hooray: 0,
-      heart: 0,
-      rocket: 0,
-      eyes: 0,
-    },
-  }
-]
+
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
 
 //createSlice function to make a reducer function
 const postsSlice = createSlice({
@@ -31,7 +14,7 @@ const postsSlice = createSlice({
   reducers: {
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
+      const existingPost = state.posts.find(post => post.id === postId)
       if (existingPost) {
         existingPost.reactions[reaction]++
       }
@@ -39,7 +22,7 @@ const postsSlice = createSlice({
 
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload)
+        state.posts.push(action.payload)
       },
      
       prepare(title, content,userId) {
@@ -66,7 +49,7 @@ const postsSlice = createSlice({
 
     postUpdated(state, action) {
       const {id,title, content} = action.payload
-      const existingPost = state.find(post=> post.id === id)
+      const existingPost = state.posts.find(post=> post.id === id)
       if (existingPost) {
         existingPost.title = title
         existingPost.content = content
