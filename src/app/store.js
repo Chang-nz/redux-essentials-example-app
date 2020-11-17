06ae66904e39c57f4a91e9fbbdd3543a1d64da29
gong hu <nzchanggong@hotmail.com>
2020-11-18 08:23:44 +1300

    11

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
 
 //configureStore()在你的react项目里，只能出现一次，记住只能出现一次！！！
 //如果出现了多次，就是违背了单一store原则。
 export default configureStore({
   reducer: {
     posts: postsReducer,         //state连到 postsReducer 再连 postsSlice
-    users: usersReducer
+    users: usersReducer,
+    notifications: notificationsReducer
   }
 })
 
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -21,6 +21,10 @@ const usersSlice = createSlice({
 
 export default usersSlice.reducer
 
+export const selectAllUsers = state => state.users
+export const selectUserById = (state,userId) => 
+  state.users.find(user => user.id === userId)
+  
 
 /*
 import { createSlice } from '@reduxjs/toolkit'
