f1e75597fa8dc7dd0317597e9ccfdf262551fc01
gong hu <nzchanggong@hotmail.com>
2020-11-18 13:25:21 +1300

    之前userId写错了

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,4 +1,4 @@
-import React, { useImperativeHandle } from 'react'
+import React from 'react'
 import {useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
 import { selectAllPosts } from '../posts/postsSlice'
@@ -8,13 +8,13 @@ import { selectUserById } from './usersSlice'
 export const UserPage = ({match}) => {
     const {userId} = match.params
 
-    const user = useSelector(state =>selectUserById(state,useImperativeHandle))
+    const user = useSelector(state =>selectUserById(state,userId))  //之前userId写错了
 
     const postsForUser = useSelector(state =>{
         const allPosts = selectAllPosts(state)
         return allPosts.filter(post =>post.user ===userId)
     })
-
+    //Link to SinglePostPage
     const postTitles =postsForUser.map(post =>(
         <li key={post.id}>
             <Link to ={`/posts/${post.id}`}>{post.title}</Link>
